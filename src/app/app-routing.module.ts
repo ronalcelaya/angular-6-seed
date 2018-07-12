87589d43b54c89e517dc1bd66fa7b3417a3d5967
Rudi <suterasutera0925@gmail.com>
2018-07-12 15:05:55 +0700

    Base config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', loadChildren: "src/app/page/login/login.page.module#LoginPageModule" },
+  { path: 'register', loadChildren: "src/app/page/register/register.page.module#RegisterPageModule" },
+  { path: 'tabpage', loadChildren: "src/app/page/page-with-tab/with-tab.page.module#WithTabPageModule" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
